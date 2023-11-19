1437f0f708d560ef9c063a24823f24fcd910b2e9
maanfinance <143881411+maanfinance@users.noreply.github.com>
2023-11-19 19:41:33 +0530

    Update db.js

--- configs/db.js
+++ configs/db.js
@@ -1,8 +1,23 @@
-const mongoose=require("mongoose")
+// const mongoose=require("mongoose")
 require('dotenv').config()
 
-mongoose.set('strictQuery', false)
-const connect=mongoose.connect(process.env.mongoUrl)
+// mongoose.set('strictQuery', false)
+// const connect=mongoose.connect(process.env.mongoUrl)
 
 
-module.exports={connect}
+// module.exports={connect}
+
+const mongoose = require('mongoose');
+
+// const dbURI = 'your_mongodb_uri_here'; // Replace with your actual MongoDB URI
+
+mongoose.connect(process.env.mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+module.exports = mongoose;
